dac7c58c87147d5c111476d4c4cbdbd1c2a24920
hamza <aheita3@gmail.com>
2024-03-04 07:09:45 +0200

    auth and login fragment

---
+++ app/src/main/java/com/hamza/ecommerce/ui/auth/login/LoginFragment.kt
@@ -0,0 +1,40 @@
+package com.hamza.ecommerce.ui.auth.login;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hamza.ecommerce.R
+import com.hamza.ecommerce.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentLoginBinding.bind(view)
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
