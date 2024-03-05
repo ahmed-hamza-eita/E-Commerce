a3e6c330a6d1fcf713f7bf9d55dd9727645a686d
hamza <aheita3@gmail.com>
2024-03-05 08:17:03 +0200

    create a preferences dataStore

---
+++ app/src/main/java/com/hamza/ecommerce/data/datasource/datastore/DataStore.kt
@@ -0,0 +1,14 @@
+package com.hamza.ecommerce.data.datasource.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+object DataStoreKeys {
+    const val E_COMMERCE_PREFERENCES = "e_commerce_preferences"
+    val IS_USER_LOGGED_IN = booleanPreferencesKey("is_user_logged_in")
+}
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DataStoreKeys.E_COMMERCE_PREFERENCES)
